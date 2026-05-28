9a2b7dbb8660d14500b0559603968a597dd9398f
Owen <owenrees.dev@gmail.com>
2026-05-28 16:28:56 +0200

    convert json string to movemap

--- lib/pgn2tex.ml
+++ lib/pgn2tex.ml
@@ -1,4 +1,17 @@
 module MoveMap = Ast.MoveMap
+open Yojson.Safe.Util
+
+let parse_diagrams_json diagrams_json_str =
+  try
+    let json = Yojson.Safe.from_string diagrams_json_str in
+    let diagrams_list = to_list json in
+    List.fold_left
+      (fun acc_map item ->
+        let ply = item |> member "ply" |> to_int in
+        let fen = item |> member "fen" |> to_string in
+        MoveMap.add ply fen acc_map)
+      MoveMap.empty diagrams_list
+  with _ -> MoveMap.empty
 
 let to_tex pgn ~diagram_data ~clock =
   let game = Parsing.parse_pgn pgn in
